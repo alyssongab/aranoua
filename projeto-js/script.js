7a0eb6d7250bc682085c393d2f432003bbeb34e0
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2024-06-05 11:10:36 -0400

    Script do botao cadastrar adicionado

--- projeto-js/script.js
+++ projeto-js/script.js
@@ -1,2 +1,17 @@
-const btnCad = document.getElementById("btn-cadastro");
+const btnCad = document.getElementById("btn-cadastrar");
+const submitCad = document.getElementById("btn-submit-cadastro");
+const livros = [["Titulo", "Autor"]];
+const titulo = document.getElementById("titulo").value;
+const autor = document.getElementById("autor").value;
 
+
+btnCad.onclick = () => {
+    const form = document.getElementById("form-cadastrar");
+    form.style.display = "block";
+}
+
+submitCad.onclick = () => {
+    for (let i = 0; i < livros[0].length; i++) {
+        
+    }
+}
