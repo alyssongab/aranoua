de64d66f6170297435df64a1c1de2111a7264564
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2024-08-16 11:02:56 -0400

    aula 2 de java poo

---
+++ java-poo/java-oo-01/src/aula01/AtendimentoMedicoComIMC.java
@@ -0,0 +1,39 @@
+package aula01;
+import java.util.Scanner;
+
+import static aula01.IMCUtil.calcularImc;
+import static aula01.IMCUtil.classificarImc;
+
+public class AtendimentoMedicoComIMC {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nome;
+        double peso;
+        double altura;
+        boolean vacinacaoEmDia;
+        String sintomas;
+        String dataAtendimento;
+
+        System.out.print("Nome: ");
+        nome = scanner.nextLine();
+
+        System.out.print("Peso: ");
+        peso = scanner.nextDouble();
+
+        System.out.print("Altura: ");
+        altura = scanner.nextDouble();
+
+        System.out.println("Vacinação em dia? ");
+        vacinacaoEmDia = scanner.nextBoolean();
+
+        System.out.print("Sintomas: ");
+        sintomas = scanner.next();
+
+        System.out.print("Data de atendimento: ");
+        dataAtendimento = scanner.next();
+
+    }
+}
---
+++ java-poo/java-oo-01/src/aula01/IMCUtil.java
@@ -0,0 +1,28 @@
+package aula01;
+
+public class IMCUtil {
+    static double calcularImc(double peso, double altura) {
+        return peso / (altura * altura);
+    }
+
+    static void classificarImc(double imc) {
+
+        String classificacao, dicas;
+
+        if (imc < 18.5) {
+            classificacao = "Magreza";
+            dicas = "Aumente a ingestão calórica com alimentos ricos em nutrientes, como frutas, legumes, proteínas magras, grãos integrais e gorduras saudáveis (abacate, nozes, azeite de oliva).";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            classificacao = "Peso normal";
+            dicas = "Mantenha uma dieta variada e equilibrada, incluindo frutas, vegetais, proteínas, carboidratos integrais e gorduras saudáveis.";
+        } else {
+            classificacao = "Sobrepeso e Obesidade";
+            dicas = "Reduza a ingestão calórica, especialmente evitando alimentos processados, ricos em açúcar e gorduras saturadas. Dê preferência a alimentos integrais, vegetais e proteínas magras.";
+        }
+
+        System.out.println();
+        System.out.format("Imc: %.2f\n", imc);
+        System.out.println("Classificacao: " + classificacao);
+        System.out.println("Dicas: " + dicas);
+    }
+}
