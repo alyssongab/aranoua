e4deac3c4186e9b89dc7c5feb90875eb75dcb189
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2024-11-18 15:59:00 -0400

    Upando arquivos do notebook

--- java_web/aranoua_java_web_aula/src/main/java/edu/ifam/TestaConexaoBD.java
+++ java_web/aranoua_java_web_aula/src/main/java/edu/ifam/TestaConexaoBD.java
@@ -10,6 +10,9 @@ public class TestaConexaoBD {
         String password = "4341";
 
         try{
+
+            Pessoa pessoa = new Pessoa("Pessoa 05", "pessoa5@ifam.edu.br", 6);
+
             Connection conexao = DriverManager.getConnection(url, user, password);
             System.out.println("Conexao realizada");
 
@@ -18,20 +21,20 @@ public class TestaConexaoBD {
             String sqlInserir = "insert into pessoa2" +
                         " (nome, email)" +
                         " values" +
-                        " ('Joao', 'joao@gmail.com')";
+                        " ('"+pessoa.getNome()+"','"+pessoa.getEmail()+"')";
 
             String sqlAlterar = "update pessoa2" +
-                                " set nome = 'Jorge'" +
-                                " where id = 2";
+                                " set nome = '"+pessoa.getNome()+"'" +
+                                " where id = "+pessoa.getId();
 
             String sqlDeletar = "delete from pessoa2" +
                                 " where id = 2";
 
             String sqlListar = "select id, nome, email from pessoa2";
 
-            System.out.println("SQL: " + sqlListar);
-            instrucao.execute(sqlListar);
-            boolean resultado = instrucao.execute(sqlListar);
+            System.out.println("SQL: " + sqlInserir);
+            instrucao.execute(sqlInserir);
+            boolean resultado = instrucao.execute(sqlInserir);
 
             if(resultado) {
                 ResultSet resultados = instrucao.getResultSet();
